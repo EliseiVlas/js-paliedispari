031017a3fd61a41cdd655d3dcadb95d1b60f12b4
Elisei <eliseivlas.002@gmail.com>
2025-01-08 16:41:20 +0100

    boiler plate (pari e dispari)

---
+++ Pari e dispari/script.js
@@ -0,0 +1,16 @@
+// FACCIAMO SCEGLIERE AL UTENTE TRA PARI O DISPARI
+
+
+// Facciamo inserire al utente un n umero da 1 a 5
+
+
+// Tramite una funzione facciamo generare un numero casule da 1 a 5 dal pc
+
+
+// Sommiamo i due numeri
+
+
+// Tramite una funzion e stabiliamo se il numero e pari o dispari
+
+
+// Dichiariamo chi ha vinto tra utente o il pc
